1727cd831b8629a461fcecb3e1c6cc90673a3922
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2019-11-09 17:35:35 +0100

    Improved CircleCI cache clearance interface

--- .circleci/clean-cache.py
+++ .circleci/clean-cache.py
@@ -21,6 +21,46 @@ def update_cache_timestamp(timestamp_filename):
         write_obj.write(str(utc_now_timestamp))
 
 
+def get_cache_to_be_cleared(valid_options, cli_args):
+    if len(cli_args) == 1:
+        cache_to_be_cleared = input(
+                'Enter which cache to be cleared. '
+                'Valid options are {}: '.format(valid_options)
+                )
+    else:
+        cache_to_be_cleared = cli_args[1]
+    return cache_to_be_cleared
+
+
+def clear_cache(valid_options, which_cache):
+    if which_cache == 'docs':
+        print('docs')
+        # update_cache_timestamp('docs-cache-timestamp')
+    elif which_cache == 'packages':
+        print('packages')
+        # update_cache_timestamp('packages-cache-timestamp')
+    elif which_cache == 'both':
+        print('both')
+        # update_cache_timestamp('docs-cache-timestamp')
+        # update_cache_timestamp('packages-cache-timestamp')
+    else:
+        err_msg = ('Invalid option. '
+                   'Valid options are: {}. '
+                   'You entered: {}'
+                   ).format(valid_options, which_cache)
+        raise ValueError(err_msg)
+
+
+def main():
+    cli_args = os.sys.argv
+    valid_options = ('docs', 'packages', 'both')
+    which_cache = get_cache_to_be_cleared(valid_options=valid_options,
+                                          cli_args=cli_args,
+                                          )
+    clear_cache(valid_options=valid_options,
+                which_cache=which_cache,
+                )
+
+
 if __name__ == '__main__':
-    update_cache_timestamp('docs-cache-timestamp')
-    update_cache_timestamp('packages-cache-timestamp')
+    main()
